3dd706c1128f8463b6c4f5b2fed09cfef6003621
UmaSankar Ramaraju <ramaraju.umasankar@gmail.com>
2013-01-27 12:07:57 -0600

    add card classes

---
+++ Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Umasankar Ramaraju on 1/27/13.
+//  Copyright (c) 2013 Umasankar Ramaraju. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Umasankar Ramaraju on 1/27/13.
+//  Copyright (c) 2013 Umasankar Ramaraju. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
